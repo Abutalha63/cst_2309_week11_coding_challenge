5182dbedfd8ea909d3d0d2be6bfa6f7d305716dd
Abutalha63 <usmanm6390@gmail.com>
2025-11-24 19:43:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 let currentSlide = 0;
 
 function showSlide(index) {
-    const slides =  document.querySelectorAll('.slide');
+  const slides = document.querySelectorAll(".slide");
 
-    slides.forEach((slide, i) => {
-        slide.style.display = i === index ? 'flex' : 'none';
-    });
+  slides.forEach((slide, i) => {
+    slide.style.display = i === index ? "flex" : "none";
+  });
 }
 
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % 6;
-    showSlide(currentSlide);
+  const slides = document.querySelectorAll(".slide");
+  currentSlide = (currentSlide + 1) % slides.length;
+  showSlide(currentSlide);
 }
 
+// show first slide immediately
+showSlide(currentSlide);
+
+// rotate every 2 seconds
 setInterval(nextSlide, 2000);
